17f92854a3ceeacb17f46e02f69636357cda4c83
Sulthanzuza <sulthanpriv@gmail.com>
2025-06-10 19:21:50 +0530

    Add ai-engine as regular folder

---
+++ ai-engine/main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from textblob import TextBlob
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://sentiment-sense-gilt.vercel.app/"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def read_root():
+    return {"message": "AI Engine Running"}
+
+@app.post("/analyze-sentiment/")
+def analyze_sentiment(text: str):
+    blob = TextBlob(text)
+    sentiment = blob.sentiment.polarity
+    return {"sentiment": sentiment}
